601a843f5883de3fda29d4465a81f78dbce881d4
Loagn545 <logan.marple25@gmail.com>
2026-07-31 21:12:55 -0400

    half of class 3

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 /// <summary>
 /// Responsible for taking input and applying it to the rigid body component of the player object.
 /// </summary>
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D), typeof(SpriteRenderer))]
+[RequireComponent (typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
@@ -13,30 +14,72 @@ public class PlayerController : MonoBehaviour
     private float jumpForce = 5f;
 
 
-    [SerializeField]
+
+    
     private Rigidbody2D rb;
+    private Collider2D col;
+    private SpriteRenderer sr;
+
+    [SerializeField]
+    private LayerMask groundLayer;
+    [SerializeField]
+    private float groundCheckRadius = 0.2f;
+
+    private Vector2 groundCheckPos => CalculateGroundCheckPos();
+    private bool isGrounded;
+
+    private Vector2 CalculateGroundCheckPos()
+    {
+        Bounds bounds = col.bounds;
+        return new Vector2(bounds.center.x, bounds.min.y);
+    }
+
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+        sr = GetComponent<SpriteRenderer>();
+        //anim = Getcomponent<Animator>();
 
         rb.linearVelocity = Vector2.zero;
+
+        //if (groundCheckTransform == null)
+        //{
+        //    Debug.LogError("Ground chwck transform is not assigned in the inspector.");
+        //    groundCheckTransform = new GameObject("GroundCheck").transform;
+         //   groundCheckTransform.SetParent(transform);
+        //    groundCheckTransform.localPosition = Vector3.zero;
+        //}
     }
 
     // Update is called once per frame
-    void Update()   
+    void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPos, groundCheckRadius, groundLayer);
+
         float horizontalInput = Input.GetAxis("Horizontal");
 
         float moveX = horizontalInput * speed;
 
         rb.linearVelocityX = moveX;
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.AddForceY(jumpForce, ForceMode2D.Impulse);
         }
+
+        SpriteFlip(horizontalInput);
     }
+
+    private void SpriteFlip(float horizontalInput) => sr.flipX = (horizontalInput < 0);
+    //{
+    //    if (sr.flipX && horizontalInput > 0 || !sr.flipX && horizontalInput < 0)
+    //    {
+    //        sr.flipX = !sr.flipX;
+    //    }
+   // }
 }
